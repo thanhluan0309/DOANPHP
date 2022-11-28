3fb9572b335260413ac30ca0ea13b2308ac2b2dc
thanhluan0309 <letranthanhluan03092001@gmail.com>
2022-11-28 13:10:11 +0700

    new update login register

--- view/login.php
+++ view/login.php
@@ -9,25 +9,11 @@
     <link rel="stylesheet" href="logincss.scss">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<script src="../callApi/userBehavior.js">
+<script src="../callApi/userCallApi.js">
 </script>
-<script>
-    const login = {
-        username: "luan123",
-        password: "123"
-    }
-    temp(login);
-</script>
+
 
 <body>
-    <!-- <h1>login</h1>
-    <?php echo "My first PHP script!";    ?>
-    <?php require('../model/user.php');
-    $user = new User("luan", "password no one known");
-    ?>
-
-    <div>User name : <?php echo $user->getName() ?></div>
-    <div>password : <?php echo $user->getpassword() ?></div> -->
 
     <div class="page">
         <div class="container">
@@ -46,12 +32,12 @@
                     <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
                 </svg>
                 <div class="form">
-                    <label for="email">Username</label>
-                    <input type="text" id="email">
+                    <label for="username">Username</label>
+                    <input type="text" id="username">
                     <label for="password">Password</label>
                     <input type="password" id="password">
                     <a href="/DOANPHP/view/register.php">Register</a>
-                    <input type="submit" id="submit" value="Submit">
+                    <input type="button" onclick="handlelogin()" id="submit" value="Submit">
                 </div>
             </div>
         </div>
@@ -59,6 +45,20 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
-
+<script>
+    const handlelogin = async () => {
+        const formlogin = {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        }
+        const res = await login(formlogin)
+        if (res.success) {
+            alert(`${res.message}`)
+            window.location.href = "http://localhost/DOANPHP/view/dashboard.php";
+        } else {
+            alert(`${res.message}`)
+        }
+    }
+</script>
 
 </html>
