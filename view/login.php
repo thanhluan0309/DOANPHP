b717df525955b8e85e77ba964aa36ca4b54f82f4
thanhluan0309 <letranthanhluan03092001@gmail.com>
2023-01-05 19:51:08 +0700

    confic merge duc, add handle

--- view/login.php
+++ view/login.php
@@ -46,7 +46,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 <script>
-    temp("asdasdas");
+    const handlelogin = async () => {
+        const formlogin = {
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        }
+        const res = await login(formlogin)
+        if (res.success) {
+            alert(`${res.message}`)
+            localStorage.setItem("token", res.accesstoken)
+            localStorage.setItem("username", res.userLogin.username)
+            window.location.href = "http://localhost/DOANPHP/view/dashboard.php";
+        } else {
+            alert(`${res.message}`)
+        }
+    }
 </script>
 
 </html>
